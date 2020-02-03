1a42cbe3893d0db85f3e1adc33250874ec4e0e05
masonxmh <55073376+masonxmh@users.noreply.github.com>
2020-02-02 16:19:48 -0800

    add the missing column in hitorical data and update DB

--- app.py
+++ app.py
@@ -1,59 +1,58 @@
 ########### Flask Application ##################
 # Dependencies and Setup
+import numpy as np
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 from collections import defaultdict
 from flask import Flask, jsonify, render_template
+# from flask_sqlalchemy import SQLAlchemy
+import pandas as pd
 
 
+# Flask Setup
+#################################################
+app = Flask(__name__)
 #################################################
 # Database Setup
 #################################################
-engine = create_engine("sqlite:///wuhan_pneumonia.sqlite")
+##app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+#app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///wuhan_pneumonia.sqlite"
 
+engine = create_engine("sqlite:///wuhan_pneumonia.sqlite")
 # reflect an existing database into a new model
 Base = automap_base()
 # reflect the tables
 Base.prepare(engine, reflect=True)
 
-# Save references to each table
-Summary = Base.classes.summary
-China_real = Base.classes.Great_China
-World_real = Base.classes.World_real
+summary = Base.classes.summary
+
 
 # Create our session (link) from Python to the DB
-session = Session(engine)
+
 
 #################################################
-# Flask Setup
-#################################################
-app = Flask(__name__)
+
 
 #################################################
 # Flask Routes
 #################################################
 @app.route("/")
-def index():
+def home():
     """Return the homepage."""
     return render_template("index.html")
 
 
-@app.route("/summary")
-def summary():
+@app.route("/test")
+def test():
     """Return a list of sample names."""
 
-    summary_result = session.query(Summary.Count, Summary.Total).all()
+    session = Session(engine)
+    results = session.query(summary.Count,summary.Total).all()
     session.close()
-
-    # Return a List of Column Names (Sample Names)
-    return jsonify(summary_result)
-
-
-
-
-
+        
+    return jsonify(results)
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ static/js/test.js
@@ -0,0 +1,9 @@
+d3.json("/test").then(function(result){
+    console.log(result[0]);
+}
+//     var deck = d3.select(".card-deck").selectAll(".card-title");
+//     deck.data(testData[1])
+//         .enter()
+//         .text(x=>x)
+    
+)
