79a74edc01a5de445889659a4d87e7633389a64f
masonxmh <55073376+masonxmh@users.noreply.github.com>
2020-02-03 17:36:14 -0800

    update China Cricles

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,23 +1,22 @@
 d3.json("/map").then(function(dataChina){
+    var circles =[];
     dataChina.forEach(element => {
-        createFeatures(dataChina);
-        console.log(element.location);})
-        console.log(dataChina);
-        // createFeatures(element);})
-     //json
-})
-
-function createFeatures(features){
-    
-    function createCircles(data){
-        
-            L.circle(data.location,{
+        // createCircles(element);
+        circles.push(L.circle(element.location,{
             color: "red",
             fillColor: "red",
             fillOpacity:0.75,
-            radius: circlesSize(data.confirmed)
-        });
-    }
+            radius: circlesSize(element.confirmed)
+            })
+        );
+    })
+    var chinaLayer = L.layerGroup(circles);
+    createMap(chinaLayer);
+
+
+    
+        
+    
     
     function circlesSize(confirmed){
         if (confirmed == 0){
@@ -25,59 +24,59 @@ function createFeatures(features){
         }else if (confirmed >=1000){
             return confirmed*15;
         }else if (confirmed >=100){
-            return confirmed*80;
+            return confirmed*150;
         } else {
             return confirmed*100;
         }
         
     }
-    var circles =[];
-    chinaGroup= circles.push(createCircles(features));
 
-    createMap(chinaGroup);
-}
 
-function createMap(chinaGroup) {
+    function createMap(chinaLayer) {
 
     
-
-    // Define variables for our tile layers
-    var light = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-    maxZoom: 18,
-    id: "mapbox.light",
-    accessToken: API_KEY
-    });
-
-    var dark = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-    maxZoom: 18,
-    id: "mapbox.dark",
-    accessToken: API_KEY
-    });
-
-    // Only one base layer can be shown at a time
-    var baseMaps = {
-    "Dark": dark,
-    "Light": light
-    };
-
-    // Overlays that may be toggled on or off
-    var overlayMaps = {
-    "China": chinaLayer
-    };
+        // Define variables for our tile layers
+        var light = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+        attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+        maxZoom: 18,
+        id: "mapbox.light",
+        accessToken: API_KEY
+        });
     
-    var chinaLayer = L.layerGroup(chinaGroup);
+        var dark = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+        attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+        maxZoom: 18,
+        id: "mapbox.dark",
+        accessToken: API_KEY
+        });
     
-    var myMap = L.map("map", {
-        center: [30.58, 114.27],
-        zoom: 5,
-        layers: [dark, chinaLayer]
+        // Only one base layer can be shown at a time
+        var baseMaps = {
+        "Dark": dark,
+        "Light": light
+        };
+    
+        // Overlays that may be toggled on or off
+        var overlayMaps = {
+        "China": chinaLayer
+        };
+        
+        
+        
+        var myMap = L.map("map", {
+            center: [30.58, 114.27],
+            zoom: 10,
+            layers: [dark, chinaLayer]
+            });
+    
+        L.control
+        .layers(baseMaps, overlayMaps,{
+            collapsed: false
+        }).addTo(myMap);
 
-    });
+    }
 
-    L.control
-    .layers(baseMaps, overlayMaps,{
-        collapsed: false
-    }).addTo(myMap);
-}
+});//json
+
+
+
